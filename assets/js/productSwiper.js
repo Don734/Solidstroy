de5db6972c03e21af5594194279d6b60564434ee
Don734 <vip.don4ik@bk.ru>
2020-11-09 18:44:59 +0500

    Project updated

--- assets/js/productSwiper.js
+++ assets/js/productSwiper.js
@@ -1,4 +1,4 @@
 
 document.addEventListener('DOMContentLoaded',Init());function Init(){productSwiper();}
 function productSwiper(){let productSwiper='.product-slider';let productSwiperOptions={spaceBetween:10,loop:false,thumbs:{swiper:productThumbSwiper()}};let productSlider=new Swiper(productSwiper,productSwiperOptions)}
-function productThumbSwiper(){let productThumbSwiper='.product-thumb-slider';let productThumbSwiperOptions={loop:false,spaceBetween:10,slidesPerView:2,watchSlidesVisibility:true,watchSlidesProgress:true,};let productThumb=new Swiper(productThumbSwiper,productThumbSwiperOptions);return productThumb;}
+function productThumbSwiper(){let productThumbSwiper='.product-thumb-slider';let productThumbSwiperOptions={loop:false,spaceBetween:10,slidesPerView:2,breakpoints:{320:{slidesPerView:1,spaceBetween:20},992:{slidesPerView:2,spaceBetween:10}},watchSlidesVisibility:true,watchSlidesProgress:true,};let productThumb=new Swiper(productThumbSwiper,productThumbSwiperOptions);return productThumb;}
